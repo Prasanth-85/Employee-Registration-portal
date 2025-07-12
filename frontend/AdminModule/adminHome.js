abd87066b543ff5f9761350a2783d9bd9dbbd960
Prasanth <prasanthtito@gmail.com>
2025-07-12 20:36:17 +0530

    Initial Commit - Employee-Registration-portal

--- frontend/AdminModule/adminHome.js
+++ frontend/AdminModule/adminHome.js
@@ -1,26 +1,26 @@
 if (localStorage.getItem("loggedIn") !== "true") {
-    window.location.href = "login.html";
+    window.location.href = "login.html"
 }
 
 function handleLogout(event) {
-    event.preventDefault();
-    alert("Logout Successfully");
-    localStorage.removeItem("loggedIn");
-    localStorage.removeItem("userName");
-    window.location.href = "login.html";
+    event.preventDefault()
+    alert("Logout Successfully")
+    localStorage.removeItem("loggedIn")
+    localStorage.removeItem("userName")
+    window.location.href = "login.html"
 }
 
 (async function fetchEmployees() {
     try {
-        const response = await fetch("http://localhost:8080/admin/fetchEmployees");
-        const employees = await response.json();
+        const response = await fetch("http://localhost:8080/admin/fetchEmployees")
+        const employees = await response.json()
 
-        const container = document.getElementById("employeeDetails");
-        container.innerHTML = "";
+        const container = document.getElementById("employeeDetails")
+        container.innerHTML = ""
 
         employees.forEach(emp => {
-            const empDiv = document.createElement("div");
-            empDiv.classList.add("employee-card");
+            const empDiv = document.createElement("div")
+            empDiv.classList.add("employee-card")
 
             empDiv.innerHTML = `
                 <p><strong class="lable">ID</strong> <span class="value" id="id-${emp.id}">${emp.id}</span></p>
@@ -36,12 +36,12 @@ function handleLogout(event) {
                 <button class="btn" onclick="editEmployee(${emp.id})">Edit</button>
                 <button class="btn"  class="value"onclick="deleteEmployee(${emp.id})">Delete</button>
                 </div>
-            `;
+            `
 
-            container.append(empDiv);
-        });
+            container.append(empDiv)
+        })
     } catch (error) {
-        console.error("Fetch Error:", error);
+        console.error("Fetch Error:", error)
     }
 })();
 
@@ -53,26 +53,26 @@ async function deleteEmployee(id) {
             method: "DELETE",
         });
 
-        const result = await response.text();
+        const result = await response.text()
 
-        alert(`Employee ${id} deleted successfully`);
-        location.reload();
+        alert(`Employee ${id} deleted successfully`)
+        location.reload()
     } catch (error) {
-        console.error("Delete Error:", error);
+        console.error("Delete Error:", error)
     }
 }
 
 // Edit employee
 function editEmployee(id) {
-    const name = document.getElementById(`name-${id}`).textContent;
-    const password = document.getElementById(`password-${id}`).textContent;
-    const email = document.getElementById(`email-${id}`).textContent;
-    const phone = document.getElementById(`phone-${id}`).textContent;
-    const status = document.getElementById(`status-${id}`).textContent;
-    const job = document.getElementById(`job-${id}`).textContent;
-    const salary = document.getElementById(`salary-${id}`).textContent;
+    const name = document.getElementById(`name-${id}`).textContent
+    const password = document.getElementById(`password-${id}`).textContent
+    const email = document.getElementById(`email-${id}`).textContent
+    const phone = document.getElementById(`phone-${id}`).textContent
+    const status = document.getElementById(`status-${id}`).textContent
+    const job = document.getElementById(`job-${id}`).textContent
+    const salary = document.getElementById(`salary-${id}`).textContent
 
-    const container = document.getElementById(`id-${id}`).parentElement.parentElement;
+    const container = document.getElementById(`id-${id}`).parentElement.parentElement
 
     container.innerHTML = `
         <p><strong class="lable" >ID</strong><span class="value"> ${id}</span></p>
@@ -87,18 +87,18 @@ function editEmployee(id) {
         <button class="btn" onclick="saveEmployee(${id})">Save</button>
         <button class="btn" onclick="location.reload()">Cancel</button>
         </div>
-        `;
+        `
 }
 
 // Save updated employee
 async function saveEmployee(id) {
-    const name = document.getElementById(`edit-name-${id}`).value;
-    const password = document.getElementById(`edit-password-${id}`).value;
-    const email = document.getElementById(`edit-email-${id}`).value;
-    const phone = Number (document.getElementById(`edit-phone-${id}`).value);
-    const status = document.getElementById(`edit-status-${id}`).value;
-    const job = document.getElementById(`edit-job-${id}`).value;
-    const salary = document.getElementById(`edit-salary-${id}`).value;
+    const name = document.getElementById(`edit-name-${id}`).value
+    const password = document.getElementById(`edit-password-${id}`).value
+    const email = document.getElementById(`edit-email-${id}`).value
+    const phone = Number (document.getElementById(`edit-phone-${id}`).value)
+    const status = document.getElementById(`edit-status-${id}`).value
+    const job = document.getElementById(`edit-job-${id}`).value
+    const salary = document.getElementById(`edit-salary-${id}`).value
 
     const updatedData = {
         name,
@@ -108,7 +108,7 @@ async function saveEmployee(id) {
         status,
         job,
         salary
-    };
+    }
 
     try {
         const response = await fetch(`http://localhost:8080/admin/updateEmployee/${id}`, {
@@ -117,17 +117,17 @@ async function saveEmployee(id) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(updatedData)
-        });
+        })
 
-        const updated = await response.json();
+        const updated = await response.json()
         console.log(updated)
         if (updated) {
-            alert("Employee updated successfully");
-            location.reload();
+            alert("Employee updated successfully")
+            location.reload()
         } else {
-            alert("Update failed");
+            alert("Update failed")
         }
     } catch (error) {
-        console.error("Update Error:", error);
+        console.error("Update Error:", error)
     }
 }
